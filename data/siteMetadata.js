1ad19e7172342e9cdd8023be5996249bba2ea961
vuesomedev <soos.gabor86@gmail.com>
2024-02-23 12:15:19 +0100

    remaining info

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,24 +1,25 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'SoniCoder Blog',
+  author: 'Gábor Soós',
+  headerTitle: 'SoniCoder',
+  description:
+    'A blog about creating and deploying well-architected production-ready Javascript applications like a professional',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
-  mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
-  threads: 'https://www.threads.net',
-  instagram: 'https://www.instagram.com',
+  // mastodon: 'https://mastodon.social/@mastodonuser',
+  email: 'sooo.gabor86@gmail.com',
+  github: 'https://github.com/sonicoder86',
+  twitter: 'https://twitter.com/sonicoder86',
+  // facebook: 'https://facebook.com',
+  // youtube: 'https://youtube.com',
+  linkedin: 'https://www.linkedin.com/in/soosgabor/',
+  // threads: 'https://www.threads.net',
+  // instagram: 'https://www.instagram.com',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
